764e00d14dc36f00d275f60add22577742f5a910
Rishabh Maru <148260712+Rishabhmaru01@users.noreply.github.com>
2025-11-12 13:45:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const btn = document.querySelector('input[type="button"]');
+const colorSelect = document.getElementById('colorSelect');
+
+btn.addEventListener('click', () => {
+  // Get the index of the selected color
+  const selectedIndex = colorSelect.selectedIndex;
+
+  // Remove the selected color if any option is selected
+  if (selectedIndex !== -1) {
+    colorSelect.remove(selectedIndex);
+  }
+});
